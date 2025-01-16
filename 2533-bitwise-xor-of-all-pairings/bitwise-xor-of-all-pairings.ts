c5674463865717c8cb453a0276d1e3c4da0684b3
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-01-16 21:52:39 +0100

    Time: 5 ms (44.44%) | Memory: 61.7 MB (88.89%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.ts
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.ts
@@ -1,8 +1,13 @@
 function xorAllNums(n1: number[], n2: number[]): number {
         let XOR_1 = 0, XOR_2 = 0;
 
-        if (n2.length % 2 != 0) for (const n of n1) XOR_1 ^= n;
-        if (n1.length % 2 != 0) for (const n of n2) XOR_2 ^= n;
+        if (n2.length % 2 != 0) 
+            for (const n of n1) 
+                XOR_1 ^= n;
+                
+        if (n1.length % 2 != 0) 
+            for (const n of n2) 
+                XOR_2 ^= n;
 
         return XOR_1 ^ XOR_2;
 };
