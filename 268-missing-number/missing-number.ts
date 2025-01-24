79427b5ae1a076347994045860b40e34c470c998
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-01-24 19:07:10 +0100

    Time: 9 ms (32.48%) | Memory: 56.7 MB (14.53%) - LeetSync

--- 268-missing-number/missing-number.ts
+++ 268-missing-number/missing-number.ts
@@ -1,8 +1,7 @@
 function missingNumber(nums: number[]): number {
     const uniq = new Set(nums);
-    const last = Math.max(...nums);
-    
-    for (let i = 0; i <= last; i++) {
+
+    for (let i = 0; i <= uniq.size; i++) {
         if (!uniq.has(i)) return i;
     }
     return uniq.size;
