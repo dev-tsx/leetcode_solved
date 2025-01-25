f389e93b973368c8d643872d7dae6f9ded1c2d80
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-01-25 19:06:40 +0100

    Time: 5 ms (62.07%) | Memory: 52.3 MB (54.02%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.ts
+++ 409-longest-palindrome/longest-palindrome.ts
@@ -4,11 +4,12 @@ function longestPalindrome(s: string): number {
     for (const char of s) {
         hash[char] = (hash[char] || 0) + 1;
     }
-    for(const key in hash){
-       hash[key] = hash[key] % 2;
+    let ones = 0;
+    for (const key in hash) {
+        hash[key] = hash[key] % 2;
+        ones += hash[key];
     }
-    const ones = Object.values(hash).filter(n => n === 1).length;
-    if(!ones || ones === 1) return s.length;
+    if (!ones || ones === 1) return s.length;
 
-    return s.length - ones + 1;  
+    return s.length - ones + 1;
 };
