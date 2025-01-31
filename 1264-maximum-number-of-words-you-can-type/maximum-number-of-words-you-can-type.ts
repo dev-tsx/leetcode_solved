12a05892fb573244477bce1e52f23ebcbcc40ab3
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-01-31 21:00:45 +0100

    Time: 2 ms (82.50%) | Memory: 51.6 MB (90.00%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.ts
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.ts
@@ -1,8 +1,6 @@
-function canBeTypedWords(text: string, brokenLetters: string): number {
-    const letters = new Set([...brokenLetters]);
+function canBeTypedWords(text: string, brokenLetters: string, letters = new Set(brokenLetters)): number {
 
     return text.split(' ').reduce((count, word) => {
-
         if (![...word].some(i => letters.has(i))) count++;
         return count
     }, 0)
