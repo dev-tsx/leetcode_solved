5e9bd06d5db2c503bb3d0889e50e543992b0dda1
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-02-01 21:56:48 +0100

    Time: 225 ms (80.00%) | Memory: 84.8 MB (0.00%) - LeetSync

--- 2003-distinct-numbers-in-each-subarray/distinct-numbers-in-each-subarray.ts
+++ 2003-distinct-numbers-in-each-subarray/distinct-numbers-in-each-subarray.ts
@@ -1,35 +1,25 @@
 function distinctNumbers(nums: number[], k: number): number[] {
-    const result: number[] = [];
-    const hash = {};
-    let uniqueCount = 0;
+    const result = [];
+    const hash = nums.slice(0, k).reduce((acc, n) => {
+        !acc.has(n) ?
+            acc.set(n, 1) :
+            acc.set(n, acc.get(n) + 1);
+        return acc;
+    }, new Map());
 
-
-    for (let i = 0; i < k; i++) {
-        if (!(nums[i] in hash)) {
-            hash[nums[i]] = 0;
-            uniqueCount++;
-        }
-        hash[nums[i]]++;
-    }
-
-    result.push(uniqueCount);
+    result.push(hash.size);
 
     for (let i = k; i < nums.length; i++) {
-        const old = nums[i - k];
-        hash[old]--;
-        if (hash[old] === 0) {
-            delete hash[old];
-            uniqueCount--;
-        }
+        const exist = nums[i - k];
+        hash.set(exist, hash.get(exist) - 1);
+        if (hash.get(exist) === 0) hash.delete(exist);
 
         const next = nums[i];
-        if (!(next in hash)) {
-            hash[next] = 0;
-            uniqueCount++;
-        }
-        hash[next]++;
+        !hash.has(next) ?
+            hash.set(next, 1) :
+            hash.set(next, hash.get(next) + 1);
 
-        result.push(uniqueCount);
+        result.push(hash.size);
     }
 
     return result;
