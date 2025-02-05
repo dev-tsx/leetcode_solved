bc9d38593c8d9399e718f75ed4f8391c101d10a7
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-02-05 22:46:09 +0100

    Time: 0 ms (100.00%) | Memory: 51.5 MB (75.56%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.ts
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.ts
@@ -7,9 +7,9 @@ function areAlmostEqual(s1: string, s2: string): boolean {
         if (s1[i] !== s2[i]) chars.push(i);
         if (chars.length > 2) return false;
     }
-    const a = chars[0], b = chars[1],
-        areTwo = a !== undefined && b !== undefined,
-        areSwapped = s1[a] === s2[b] && s1[b] === s2[a];
+    const indx1 = chars[0], indx2 = chars[1],
+        areTwo = indx1 !== undefined && indx2 !== undefined,
+        areSwapped = s1[indx1] === s2[indx2] && s1[indx2] === s2[indx1];
 
-    return areTwo && areSwapped
+    return areTwo && areSwapped;
 }
