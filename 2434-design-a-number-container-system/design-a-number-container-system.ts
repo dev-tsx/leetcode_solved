19de667c217e997060259e5a01a13f0fb4316e62
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-02-08 20:07:25 +0100

    Time: 114 ms (100.00%) | Memory: 100.8 MB (100.00%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.ts
+++ 2434-design-a-number-container-system/design-a-number-container-system.ts
@@ -90,13 +90,3 @@ class NumberContainers {
         return -1;
     }
 }
-
-
-
-
-/**
- * Your NumberContainers object will be instantiated and called as such:
- * var obj = new NumberContainers()
- * obj.change(index,number)
- * var param_2 = obj.find(number)
- */
