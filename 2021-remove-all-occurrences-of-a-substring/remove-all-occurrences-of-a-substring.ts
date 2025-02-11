37a8325634c6a1e5ca9a745602bf591473c84cf1
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-02-12 00:01:19 +0100

    Time: 2 ms (62.50%) | Memory: 52.4 MB (33.33%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.ts
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.ts
@@ -1,7 +1,7 @@
 function removeOccurrences(s: string, part: string): string {
     while (s.includes(part)) {
         const idx = s.indexOf(part);
-        s = s.substring(0, idx) + s.substring(idx + part.length);
+        s = s.slice(0, idx) + s.slice(idx + part.length);
     }
     return s;
 }
