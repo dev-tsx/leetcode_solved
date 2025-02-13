47ac0804a3bb472fca90f9b45abaac220da7b0bb
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-02-13 21:41:01 +0100

    Time: 96 ms (45.45%) | Memory: 70.4 MB (25.86%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.ts
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.ts
@@ -1,17 +1,23 @@
 function maximumSum(nums: number[]): number {
-    const hashSum: Record<number, number[]> = {};
+    const hashSum = {};
+    let maxSum = -1;
 
     for (const num of nums) {
         const sum = [...String(num)].reduce((acc, n) => acc + Number(n), 0);
 
-        hashSum[sum] ||= [0, 0];
+        let [a = 0, b = 0] = hashSum[sum] || [];
 
-        if (num > hashSum[sum][0]) {
-            hashSum[sum][1] = hashSum[sum][0];
-            hashSum[sum][0] = num;
-        } else if (num > hashSum[sum][1]) {
-            hashSum[sum][1] = num;
+        if (num > a) {
+            b = a;
+            a = num;
+        } else if (num > b) {
+            b = num;
         }
+
+        hashSum[sum] = [a, b];
+
+        if (b > 0) maxSum = Math.max(maxSum, a + b);
     }
-    return Math.max(...Object.values(hashSum).map(([a, b]) => b > 0 ? a + b : -1)) || -1
+
+    return maxSum;
 }
