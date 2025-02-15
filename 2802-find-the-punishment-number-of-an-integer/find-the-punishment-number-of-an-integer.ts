826853bfebaf69da2e755e2c69c483f97cf5e7af
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-02-15 18:01:59 +0100

    Time: 56 ms (33.33%) | Memory: 57.6 MB (33.33%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.ts
@@ -0,0 +1,29 @@
+function punishmentNumber(n: number): number {
+
+    let total = 0;
+
+    for (let i = 1; i <= n; i++) {
+        const squared = i * i;
+        if (canPartition(squared, i, 0)) {
+            total += squared;
+        }
+    }
+
+    return total;
+}
+function canPartition(num: number, target: number, index: number): boolean {
+    const str = num.toString();
+
+    function backtrack(i: number, sum: number): boolean {
+        if (i === str.length) return sum === target;
+        let currentSum = 0;
+        for (let j = i; j < str.length; j++) {
+            currentSum = currentSum * 10 + Number(str[j]);
+            if (backtrack(j + 1, sum + currentSum)) return true;
+        }
+        return false;
+    }
+
+    return backtrack(0, 0);
+}
+
