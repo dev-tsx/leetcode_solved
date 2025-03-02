430f49ee051e9eb9ecc64dae38cf0721acff8b09
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-03-02 21:33:52 +0100

    Time: 7 ms (33.33%) | Memory: 60.9 MB (9.33%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.js
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.js
@@ -4,16 +4,17 @@
  * @return {number[][]}
  */
 var mergeArrays = function (nums1, nums2) {
-    const hash = nums1.reduce((acc, [id, val]) => {
-        acc[id] ||= 0;
-        acc[id] += val;
-        return acc;
-    }, {});
-    for (const [id, val] of nums2) {
-        hash[id] ||= 0;
-        hash[id] += val;
+    const hash = {};
+
+    for (let i = 0; i < nums1.length; i++) {
+        const [id, val] = nums1[i];
+        hash[id] = (hash[id] || 0) + val;
+    }
+    for (let i = 0; i < nums2.length; i++) {
+        const [id, val] = nums2[i];
+        hash[id] = (hash[id] || 0) + val;
     }
 
     const result = Object.keys(hash).map(id => [+id, hash[id]]);
-    return result
+    return result.sort((a, b) => a - b)
 };
