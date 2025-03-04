30aa4ce889484e43a9c8ea1f3fdb8e9b054bd773
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-03-04 19:54:59 +0100

    Time: 0 ms (100.00%) | Memory: 53.3 MB (22.73%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var checkPowersOfThree = function (n) {
+    let power = 0;
+
+    while (Math.pow(3, power) <= n) {
+        power++;
+    }
+
+    while (n > 0) {
+        if (n >= Math.pow(3, power)) {
+            n -= Math.pow(3, power);
+        }
+        if (n >= Math.pow(3, power)) {
+            return false;
+        }
+        power--;
+    }
+    return true;
+};
