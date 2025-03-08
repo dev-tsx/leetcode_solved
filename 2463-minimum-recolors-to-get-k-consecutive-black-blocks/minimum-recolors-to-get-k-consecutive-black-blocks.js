1259ad608d68b71f38fd232870b7d288391bab12
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-03-08 13:55:08 +0100

    Time: 2 ms (48.60%) | Memory: 54.8 MB (22.43%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.js
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.js
@@ -11,7 +11,6 @@ var minimumRecolors = function (blocks, k) {
     for (let i = 0; i < blocks.length - k + 1; i++) {
         subblocks.push(blocks.slice(i, i + k));
     }
-    const blackCount = subblocks.map(item => item.replaceAll('W', ""));
-    console.log(blackCount);
-    return Math.max(0, k - Math.max(...subblocks.map(s => s.replaceAll('W', "").length)))
+    const blackCount = subblocks.map(item => item.replaceAll('W', "").length);
+    return Math.max(0, k - Math.max(...blackCount))
 };
