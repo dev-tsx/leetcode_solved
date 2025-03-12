54b398d585f926c77a5df6fc15db498db60e65f2
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-03-12 23:33:19 +0100

    Time: 2 ms (21.43%) | Memory: 58.7 MB (7.14%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.ts
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.ts
@@ -1,6 +1,9 @@
 function maximumCount(nums: number[]): number {
-    const withoutZero = nums.filter(a => a !== 0);
-    const positive = withoutZero.filter(a => a > 0).length;
-    const negative = withoutZero.length - positive;
+    let positive = 0, negative = 0;
+    for (const n of nums) {
+        if (n === 0) continue;
+        else if (n < 0) negative++;
+        else positive++;
+    }
     return Math.max(positive, negative);
 };
