a4947981a9d75b2714c0c290ac34d37e3a3659f6
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-04-01 21:21:06 +0200

    Time: 2 ms (67.97%) | Memory: 60.9 MB (37.54%) - LeetSync

--- 55-jump-game/jump-game.ts
+++ 55-jump-game/jump-game.ts
@@ -1,9 +1,10 @@
 function canJump(nums: number[]): boolean {
-    let maxJump = 0, lastIndx = nums.length - 1;
+    let maxJump = 0;
 
-    for (let i = 0; i <= maxJump; i++) {
-        if (maxJump < nums[i] + i) maxJump = nums[i] + i;
-        if (maxJump >= lastIndx) return true;
+    for (let i = 0; i < nums.length; i++) {
+        if (maxJump < i) return false;
+        maxJump = Math.max(maxJump, nums[i] + i);
+        if (maxJump >= nums.length - 1) return true;
     }
     return false;
 };
