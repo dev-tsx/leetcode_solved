67900d70bffb67afcf275b7ef479630824503ef3
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-04-18 23:55:25 +0200

    Time: 5 ms (70.85%) | Memory: 55.6 MB (80.86%) - LeetSync

---
+++ 38-count-and-say/count-and-say.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function (n) {
+    let str = "1";
+    for (let i = 2; i <= n; i++) {
+        let next = "";
+        for (let j = 0, k = 0; j < str.length; j = k) {
+            while (k < str.length && str[k] == str[j]) k++;
+            next += (k - j).toString() + str[j];
+        }
+        str = next;
+    }
+    return str;
+};
