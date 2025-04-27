729c613f368e276d945f92c5fe17df46866832f8
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-04-27 20:50:38 +0200

    Time: 2 ms (42.67%) | Memory: 55.7 MB (94.67%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.js
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.js
@@ -1,5 +1,4 @@
 const countSubarrays = nums => nums.reduce((counter, num, i) => {
     if (i > nums.length - 2) return counter;
-    counter += num + nums[i + 2] === nums[i + 1] / 2 ? 1 : 0;
-    return counter
+    return (counter += num + nums[i + 2] === nums[i + 1] / 2 ? 1 : 0);
 }, 0)
