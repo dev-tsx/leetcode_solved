2c534211bf289590a47ceebe2aa103c80de9a362
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-04-30 22:02:13 +0200

    Time: 1 ms (83.28%) | Memory: 57 MB (77.70%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.ts
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.ts
@@ -1,6 +1,1 @@
-function findNumbers(nums: number[]): number {
-    return nums.reduce((count, num) => {
-        if (num.toString().length % 2 == 0) count++;
-        return count;
-    }, 0)
-};
+const findNumbers = (m: number[]): number => m.reduce((acc, n) => (acc + +(("" + n).length % 2 == 0)), 0)
