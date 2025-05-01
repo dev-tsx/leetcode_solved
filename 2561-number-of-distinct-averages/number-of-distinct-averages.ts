ac1ccc5a6e38742b6c4534ff6e903d045ecb4f32
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-05-01 22:51:32 +0200

    Time: 0 ms (100.00%) | Memory: 54.1 MB (95.71%) - LeetSync

--- 2561-number-of-distinct-averages/number-of-distinct-averages.ts
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.ts
@@ -1,9 +1,11 @@
 function distinctAverages(nums: number[]): number {
-    nums.sort((a,b) => a - b);
+    nums.sort((a, b) => a - b);
     let distinct = new Set();
-
-    while(nums.length){
-        distinct.add((nums.pop() + nums.shift())/2);
-    }    
+    let min = 0; let max = nums.length - 1;
+    while (min < max) {
+        distinct.add((nums[min] + nums[max]) / 2);
+        min++;
+        max--;
+    }
     return distinct.size
 };
