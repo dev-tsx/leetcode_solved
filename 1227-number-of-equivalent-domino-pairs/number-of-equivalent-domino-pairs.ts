dc8988f318ca3a891a47ca5a81a8b1fa6807cf61
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-05-04 18:10:05 +0200

    Time: 15 ms (65.00%) | Memory: 65.7 MB (90.00%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.ts
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.ts
@@ -1,13 +1,7 @@
-function numEquivDominoPairs(dominoes: number[][]): number {
-    const map = {};
-    let pairs = 0;
-
-    for (const [a, b] of dominoes) {
-        const key = a <= b ? `${a},${b}` : `${b},${a}`;
-        const count = map[key] || 0;
-        pairs += count;
-        map[key] = count + 1;
-    }
-
-    return pairs;
-}
+const numEquivDominoPairs = (dominoes: number[][]): number => dominoes.reduce((map, [a, b]) => {
+    const key = a <= b ? `${a},${b}` : `${b},${a}`;
+    const count = map[key] || 0;
+    map.pairs += count;
+    map[key] = count + 1;
+    return map;
+}, { pairs: 0 }).pairs
