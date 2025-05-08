4fca927820fc8d89b74e50bce8823739f564e91b
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-05-09 00:28:14 +0200

    Time: 1 ms (4.55%) | Memory: 53.7 MB (95.45%) - LeetSync

--- 266-palindrome-permutation/palindrome-permutation.ts
+++ 266-palindrome-permutation/palindrome-permutation.ts
@@ -1,5 +1,8 @@
 function canPermutePalindrome(s: string): boolean {
-    const hash: Record<string, number> = {};
-    for (const char of s) hash[char] = (hash[char] || 0) + 1;
-    return Object.values(hash).filter(v => v % 2 != 0).length <= 1;
+    const chars = Array.from({ length: 26 }, () => 0);
+    for (const char of s) {
+        const i = 'z'.charCodeAt(0) - char.charCodeAt(0);
+        chars[i]++;
+    }
+    return chars.filter(v => v % 2 != 0).length <= 1;
 };
