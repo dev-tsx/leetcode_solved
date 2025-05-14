0a810b579e93ab53b24f97dd2da536f364247b90
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-05-14 22:49:03 +0200

    Time: 3 ms (25.34%) | Memory: 56.2 MB (26.58%) - LeetSync

--- 202-happy-number/happy-number.js
+++ 202-happy-number/happy-number.js
@@ -1,16 +1,16 @@
-/**
- * @param {number} n
- * @return {boolean}
- */
-
 const sumOfDigitsPower = (num) => [..."" + num].reduce((sum, digit) => {
     sum += Number(digit) ** 2;
     return sum;
 }, 0)
 
-var isHappy = function (n, seen = new Set()) {
-    if (n == 1) return true;
-    if (seen.has(n)) return false;
-    seen.add(n);
-    return isHappy(sumOfDigitsPower(n), seen);
+var isHappy = function (n) {
+    let slow = n;
+    let fast = sumOfDigitsPower(n);
+
+    while (true) {
+        if (fast == 1) return true;
+        if (fast == slow) return false;
+        fast = sumOfDigitsPower(sumOfDigitsPower(fast));
+        slow = sumOfDigitsPower(slow);
+    }
 };
