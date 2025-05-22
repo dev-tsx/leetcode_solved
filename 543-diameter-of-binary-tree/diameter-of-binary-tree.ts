7fc790f745471195982ded76140acf6c731d3ce4
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-05-22 23:13:30 +0200

    Time: 4 ms (18.96%) | Memory: 64.2 MB (22.50%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.ts
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.ts
@@ -1,17 +1,24 @@
+class Pair {
+    diameter: number;
+    height: number;
+    constructor(diameter: number, height: number) {
+        this.diameter = diameter;
+        this.height = height;
+    }
+}
 function diameterHelper(node) {
-    if (node == null) return { h: 0, d: 0 };
+    if (node == null) return new Pair(0, 0);
 
     const lh = diameterHelper(node.left);
     const rh = diameterHelper(node.right);
 
-    const height = Math.max(lh.h, rh.h) + 1;
-    const diameter = Math.max(lh.d, rh.d, lh.h + rh.h);
-
-    return { h: height, d: diameter };
+    const height = Math.max(lh.height, rh.height) + 1;
+    const diameter = Math.max(lh.diameter, rh.diameter, lh.height + rh.height);
+    return new Pair(diameter, height);
 }
 
 function diameterOfBinaryTree(root: TreeNode | null): number {
     if (root == null) return 0;
-
-    return diameterHelper(root).d;
+    const pair = diameterHelper(root);
+    return pair.diameter
 };
