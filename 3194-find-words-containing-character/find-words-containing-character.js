bf735c699ce8b1536eaef063a12828b6ce36e5b3
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-05-24 22:03:21 +0200

    Time: 2 ms (38.91%) | Memory: 59.2 MB (11.48%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -3,4 +3,4 @@
  * @param {character} x
  * @return {number[]}
  */
-const findWordsContaining = (w, x) => w.map((e, i) => [e.includes(x), i]).filter(e => e[0]).map(e => e[1])
+const findWordsContaining = (w, x) => w.flatMap((e, i) => e.includes(x) ? [i] : []);
