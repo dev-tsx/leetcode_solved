4dc6afb7a5afb0eead3162255b78215cfc321266
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-05-26 22:07:30 +0200

    Time: 0 ms (100.00%) | Memory: 59.3 MB (92.31%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.ts
@@ -0,0 +1,8 @@
+var largestOddNumber = function (num: string): string {
+    for (let i = num.length - 1; i >= 0; i--) {
+        if (parseInt(num[i]) % 2 === 1) {
+            return num.slice(0, i + 1);
+        }
+    }
+    return "";
+};
