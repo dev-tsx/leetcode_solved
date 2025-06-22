c49518e303657804b331345cfdf76142edbea5f0
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-06-22 22:24:35 +0200

    Time: 0 ms (100.00%) | Memory: 54.1 MB (25.53%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.js
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.js
@@ -4,19 +4,14 @@
  * @param {character} fill
  * @return {string[]}
  */
-var divideString = function(s, k, fill) {
+var divideString = function (s, k, fill) {
     const r = [];
     let i = 0;
-    let item = s.slice(i,k);
-    while(item.length){
-        if(item.length === k){
-            r.push(item);
-        } else {
-           const diff = k - item.length;
-           r.push(item + fill.repeat(diff));
-        }
-        i+=k;
-        item = s.slice(i,k + i);
+    let item = s.slice(i, k);
+    while (item.length) {
+        r.push(item.length === k ? item : item + fill.repeat(k - item.length))
+        i += k;
+        item = s.slice(i, k + i);
     }
     return r
 };
