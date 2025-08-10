68c31cf6e5948b3e98eb3654cd2fe9fd600f7eee
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-08-10 21:50:46 +0200

    Time: 1 ms (77.78%) | Memory: 55.5 MB (66.67%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.js
+++ 900-reordered-power-of-2/reordered-power-of-2.js
@@ -8,7 +8,7 @@ const numToSortedString = (n) => [...String(n)].sort((a, b) => b - a).join('');
 const reorderedPowerOf2 = (n) => {
     const value = numToSortedString(n);
     for (let i = 0; i < 30; i++) {
-        const sortedPow = numToSortedString(numToSortedString(2 ** i));
+        const sortedPow = numToSortedString(2 ** i);
         if (sortedPow == value) {
             return true;
         }
