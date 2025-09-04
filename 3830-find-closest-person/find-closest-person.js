853870c780ebbaf14d31a6572a9330a3397fa465
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-09-05 00:24:06 +0200

    Time: 0 ms (100.00%) | Memory: 55.4 MB (41.13%) - LeetSync

--- 3830-find-closest-person/find-closest-person.js
+++ 3830-find-closest-person/find-closest-person.js
@@ -1,12 +1,6 @@
-/**
- * @param {number} x
- * @param {number} y
- * @param {number} z
- * @return {number}
- */
-var findClosest = function (x, y, z) {
-    const diff1 = Math.abs(z - x);
-    const diff2 = Math.abs(z - y);
+const findClosest = (x, y, z) => {
+    const d1 = Math.abs(z - x);
+    const d2 = Math.abs(z - y);
 
-    return diff1 == diff2 ? 0 : diff1 < diff2 ? 1 : 2;
+    return d1 == d2 ? 0 : d1 < d2 ? 1 : 2;
 };
