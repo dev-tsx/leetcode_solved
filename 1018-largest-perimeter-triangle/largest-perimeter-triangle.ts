d5ee96cad082627260de0596511f1979b6d6a341
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2025-09-29 01:39:34 +0200

    Time: 25 ms (60.34%) | Memory: 59.7 MB (15.52%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.ts
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.ts
@@ -2,9 +2,14 @@ function largestPerimeter(nums: number[]): number {
 
     nums.sort((a, b) => a - b);
     for (let i = nums.length - 3; i >= 0; --i) {
-        if (nums[i] + nums[i + 1] > nums[i + 2])
-            return nums[i] + nums[i + 1] + nums[i + 2];
-    }
+        const side1 = nums[i];
+        const side2 = nums[i + 1];
+        const side3 = nums[i + 2];
 
+        if (side1 + side2 > side3) {
+            return side1 + side2 + side3
+        }
+
+    }
     return 0;
 };
