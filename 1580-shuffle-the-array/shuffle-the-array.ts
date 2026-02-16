f03d2a40c824145c5b47c653e7ae30dbc8848572
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2026-02-16 23:52:06 +0100

    Time: 52 ms (61.62%) | Memory: 58.3 MB (80.49%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.ts
+++ 1580-shuffle-the-array/shuffle-the-array.ts
@@ -1,9 +1,12 @@
 function shuffle(nums: number[], n: number): number[] {
-const result = [];
-
-for(let i = 0; i < nums.length/2;i++){
-    result.push(nums[i]);
-    result.push(nums[i + n]);
+const result:number[] = Array.from({length:n * 2}).map(j => 0);
+let i = 0;
+let j = 0;
+while(nums[i + n] !== undefined){
+    result[j] = nums[i];
+    result[j + 1] = nums[i + n];
+    i++;
+    j = j + 2;
 }
 return result;
 };
