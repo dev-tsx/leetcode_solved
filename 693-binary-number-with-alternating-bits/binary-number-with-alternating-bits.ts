efc0dc3192d202037c29f730c551c7f9c2bb0d35
Senior Software Engineer <vlozghachov.datastructure@gmail.com>
2026-02-18 22:20:14 +0100

    Time: 0 ms (100.00%) | Memory: 55.5 MB (50.00%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.ts
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.ts
@@ -1,6 +1,6 @@
 function hasAlternatingBits(n: number): boolean {
     if (n == 3) return false;
-    if(n == 1) return true;
+    if (n == 1) return true;
     const bin = n.toString(2);
     for (let i = 1; i < bin.length - 1; i++) {
         if (bin[i] == bin[i - 1] || bin[i] == bin[i + 1])
